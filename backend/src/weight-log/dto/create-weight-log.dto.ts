4554a7fcade3c41571a7025e27ee367819618bb8
goerkem <goerkem@p200300e5f736fd73434f774bf2bc8710.dip0.t-ipconnect.de>
2026-06-17 13:56:26 +0200

    feat: add @ApiProperty decorator to CreateWeightLogDto

--- backend/src/weight-log/dto/create-weight-log.dto.ts
+++ backend/src/weight-log/dto/create-weight-log.dto.ts
@@ -1,6 +1,8 @@
 import { IsNumber, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateWeightLogDto {
+  @ApiProperty({ description: 'Weight in kilograms', minimum: 20 })
   @IsNumber()
   @Min(20)
   weightKg!: number;
