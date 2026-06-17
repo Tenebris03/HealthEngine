0998b6aacedbb9a775ce11629a4c8678851f9963
goerkem <goerkem@p200300e5f736fd73434f774bf2bc8710.dip0.t-ipconnect.de>
2026-06-17 14:08:04 +0200

    feat: implement leaderboard page with table, filter, route, and i18n

--- frontend/src/types/i18n.d.ts
+++ frontend/src/types/i18n.d.ts
@@ -4,6 +4,7 @@ import keyFeaturesEn from '../features/key-features/locales/en/key-features.json
 import goalsEn from '../features/goals/locales/en/goals.json';
 import progressEn from '../features/progress/locales/en/progress.json';
 import dashboardEn from '../assets/locales/en/dashboard.json';
+import leaderboardEn from '../features/leaderboard/locales/en/leaderboard.json';
 
 interface Resources {
   global: typeof globalEn;
@@ -12,6 +13,7 @@ interface Resources {
   goals: typeof goalsEn;
   progress: typeof progressEn;
   dashboard: typeof dashboardEn;
+  leaderboard: typeof leaderboardEn;
 }
 
 declare module 'i18next' {
